4af11036c888d02c437941939b00b597b8323480
MAshraf <ee.mashraf@gmail.com>
2018-09-16 08:49:15 +0300

    l02

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.use("/public", express.static(__dirname + '/public'))
 // Lessons
 const lessons = {
                     '01' : 'Fancy Button',
+                    '02' : 'Sexy Typography',
 
                   }
 
